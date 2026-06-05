67ba684f0feb48b67f9b86d700e0e21de567fa08
ichika <pranavjirel@gmail.com>
2026-06-05 11:33:07 +0545

    ambxst backup - 2026-06-05 11:33

--- harman-restore.sh
+++ harman-restore.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 echo "restoring harman rice..."
 
+pkill -f ambxst
+pkill quickshell
+sleep 1
+
+
 cp -r ~/dotfiles/harman/hypr ~/.config/
 cp -r ~/dotfiles/harman/quickshell ~/.config/
 cp -r ~/dotfiles/harman/swaync ~/.config/
 hyprctl reload
 wal -R
+quickshell &
 
 echo "done! harman restored"
 
