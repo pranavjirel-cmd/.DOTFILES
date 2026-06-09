3cf313ea4ba32ee74b28a2a6a94dbc09e8ff7339
ichika <pranavjirel@gmail.com>
2026-06-09 20:23:57 +0545

    harman backup - 2026-06-09 20:23

--- harman-restore.sh
+++ harman-restore.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 echo "restoring harman rice..."
 
-pkill swww-daemon
-pkill awww-daemon
-sleep 1
-awww-daemon &
-sleep 1
-pkill -f "src/ambxst"
-pkill quickshell
-pkill axctl
-sleep 2
+pkill -x qs 2>/dev/null
+pkill -x quickshell 2>/dev/null
 
-cp ~/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf.bak
 cp -r ~/dotfiles/harman/hypr ~/.config/
 cp -r ~/dotfiles/harman/quickshell ~/.config/
 cp -r ~/dotfiles/harman/swaync ~/.config/
-sed -i '/source = ~\/.local\/share\/ambxst\/hyprland.conf/d' ~/.config/hypr/hyprland.conf
-sed -i '107,189s/^#bind/bind/; 107,189s/^#binde/binde/; 107,189s/^#bindm/bindm/' ~/.config/hypr/hyprland.conf
 hyprctl reload
-wal -R
-awww img $(cat ~/.cache/wal/wal) --transition-type wipe --transition-duration 1
-sleep 1
-quickshell &
+
+swww-daemon 2>/dev/null &
+quickshell 2>/dev/null &
+swaync 2>/dev/null &
+
+sleep 2
+WALL=~/wallpapers/Girl-Short-Hair.jpg
+quickshell ipc call randomwallpaper apply "$WALL"
+wal -i "$WALL" --saturate 1.0
 
 echo "done! harman restored"
