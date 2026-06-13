e02428c04895f6f054f65c3e7f588eb5655e29ee
ichika <pranavjirel@gmail.com>
2026-06-13 14:11:27 +0545

    harman backup - 2026-06-13 14:11

--- harman-backup.sh
+++ harman-backup.sh
@@ -2,6 +2,7 @@
 echo "backing up harman rice..."
 
 cp ~/.config/hypr/hyprland.conf ~/dotfiles/harman/hypr/
+cp ~/.config/hypr/harman-keybinds.conf ~/dotfiles/harman/hypr/
 cp ~/.config/hypr/hyprlock.conf ~/dotfiles/harman/hypr/
 cp -r ~/.config/quickshell ~/dotfiles/harman/
 cp -r ~/.config/swaync ~/dotfiles/harman/
